aa8dfd9daeae2980e30289c6578b50828006519d
juliamendoim <juliamilanese@gmail.com>
2025-01-08 08:26:12 -0300

    small cleanup

--- src/helpers.py
+++ src/helpers.py
@@ -2,7 +2,6 @@
 import re
 
 def matching_section(text: str) -> bool:
-
         pattern = r'\b(?:endpoint|endpoints|objective|objectives)\b'
 
         return re.search(pattern, text.lower()) is not None
